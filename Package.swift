e4dea8dc47801b0be16e615b8dbc431f9083e380
kakaosdk <kakaosdk.dev@kakaocorp.com>
2025-04-22 19:02:41 +0900

    2.24.1

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-// sdk-version:2.24.0
+// sdk-version:2.24.1
 
 import PackageDescription
 
@@ -33,11 +33,11 @@ let package = Package(
     dependencies: [
         .package(name: "RxKakaoOpenSDK",
                  url: "https://github.com/kakao/kakao-ios-sdk-rx.git",
-                 .exact("2.24.0")
+                 .exact("2.24.1")
                 ),
         .package(name: "KakaoPartnerSDK",
                  url: "https://github.com/kakao/kakao-partner-ios-sdk.git",
-                 .exact("2.24.0")
+                 .exact("2.24.1")
                 )
     ],
     targets: [
